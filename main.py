ee4f1b64d0225270224a521cd3c8749b5e38ff51
Naquan A. A <bradshawn@hartwick.edu>
2020-09-03 13:23:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This program says hello and ask for my name.
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('How old are you?')
+myAge = input()
+print('what are your hobbies?')
+Playingsports = input()
+print('Are you planning on playing a sport in college?')
+Yesiamonthefootballteam = input()
+print('Whats are your dream jobs?')
+Becomingaphysicaleducationteacher = input()
